bf4c8c44f859ecd0d17bd4ec233728e758e6cad6
LinneaMay <Linneamay@gilbertson.net>
2025-03-20 16:21:43 -0500

    making tryFrom for packets

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-fn main() {
-    println!("Hello, world!");
-}
 
 // Below is a version of the `main` function and some error types. This assumes
 // the existence of types like `FileManager`, `Packet`, and `PacketParseError`.
 // You can use this code as a starting point for the exercise, or you can
 // delete it and write your own code with the same function signature.
 
-/*
+
 
 use std::{
-    io::{self, Write},
-    net::UdpSocket,
+    ffi::OsString, io::{self, Write}, net::UdpSocket, os::unix::ffi::OsStringExt
 };
 
 #[derive(Debug)]
@@ -31,6 +27,61 @@ impl From<PacketParseError> for ClientError {
         Self::PacketParseError(e)
     }
 }
+
+#[derive(Debug)]
+pub enum PacketParseError {
+
+}
+
+pub enum Packet {
+    Header(Header),
+    Data(Data)
+}
+
+pub struct Header {
+    file_id: u8,
+    file_name: OsString
+}
+
+pub struct Data {
+    file_id: u8,
+    packet_number: u16,
+    is_last_packet: bool,
+    data: Vec<u8>
+}
+
+
+impl TryFrom<&[u8]> for Packet {
+    type Error = PacketParseError;
+
+    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+
+        if buffer[0] % 2 == 0 {  //header packet
+
+            if buffer.len < 4
+
+            let file_name= OsString::from_vec(buffer[2..].to_vec());
+
+            let file_id = buffer[1];
+
+            Ok(Packet::Header(Header { file_id, file_name }))
+        }
+        else {
+            let file_id = buffer[1];
+            let packet_number_bytes = [buffer[2], buffer[3]];
+            let packet_number = u16::from_be_bytes(packet_number_bytes);
+
+            let is_last_packet = buffer[0] % 4 == 3;
+            let data = buffer[4..].to_vec();
+
+            Ok(Packet::Data(Data { file_id, packet_number, is_last_packet, data}))
+
+        }
+
+    }
+}
+
+
 
 fn main() -> Result<(), ClientError> {
     let sock = UdpSocket::bind("0.0.0.0:7077")?;
@@ -56,4 +107,5 @@ fn main() -> Result<(), ClientError> {
     Ok(())
 }
 
- */
+
+
